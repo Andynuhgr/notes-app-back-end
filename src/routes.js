35a29cd2676eddb043a1d9b6be41220003bae2ee
Andy Nuhgroho <andynara06@gmail.com>
2023-03-22 10:01:55 +0700

    initial commit

---
+++ src/routes.js
@@ -0,0 +1,38 @@
+// eslint-disable-next-line max-len
+const {
+  addNoteHandler,
+  getAllNotesHandler,
+  getNoteBYIdhandler,
+  editNoteByHandler,
+  deleteNoteByIdHandler,
+} = require('./handler');
+
+const routes = [
+  {
+    method: 'POST',
+    path: '/notes',
+    handler: addNoteHandler,
+  },
+  {
+    method: 'GET',
+    path: '/notes',
+    handler: getAllNotesHandler,
+  },
+  {
+    method: 'GET',
+    path: '/notes/{id}',
+    handler: getNoteBYIdhandler,
+  },
+  {
+    method: 'PUT',
+    path: '/notes/{id}',
+    handler: editNoteByHandler,
+  },
+  {
+    method: 'DELETE',
+    path: '/notes/{id}',
+    handler: deleteNoteByIdHandler,
+  },
+];
+
+module.exports = routes;
